5c373dbee77058cae9d58a5537ed292b286b4b0e
Abai10 <aboodahmedai010@googlemail.com>
2025-07-09 23:06:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ async def main():
     """Creates an aiohttp session and fetches multiple URLs concurrently."""
     urls = list()
     apis =[
-        "wyAKEVj7TXKodgQllpM5qAS7VZX8Vxnm", "cMOt6PpDno4TwTIyFtgOCEgQxaWNnwAg"]
+        "ZNfBb7aINljzZnwJx3fMwCITSVqRh2w2", "2zs4lAzX8tmfK4t87FbH4OaOVLLa3Iuc"]
     #ad ="https://www.effectiveratecpm.com/ca83bzpx98?key=dee9c6f3171b614287718132222041ad"
     #ad = "https://www.profitableratecpm.com/zhzbtigdvk?key=bfdf77a1bedb6a88e866ad888aa3896b"
     ad ="https://www.profitableratecpm.com/e448i84bx?key=c52b15369f8988b9a0c8a7dbdfbe84f4"
     ad = "https://www.profitableratecpm.com/mrrsnt73cz?key=a9264f3ab1770607dd37951c2da5963c"
     for api in apis:
         for _ in range(randint (3,5)):
-            url = f"https://api.webscrapingapi.com/v2?api_key={api}&url={ad}&country=uk&render_js=1"
+            url = f"https://api.webscrapingapi.com/v2?api_key={api}&url={ad}&country=fr&render_js=1"
             urls.append(url)
 
     async with aiohttp.ClientSession() as session:
